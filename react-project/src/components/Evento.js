fa989936016f7e5d861f33bfd11af26e7ab0de29
Alessandro <alessandro5@live.com>
2024-04-17 22:27:30 -0300

    feat: second Event

--- react-project/src/components/Evento.js
+++ react-project/src/components/Evento.js
@@ -4,11 +4,16 @@ function Evento(){
     function meuEvento(){
         console.log("Opa, ativação realizada!")
     }
+    
+    function segundoEvento(){
+        console.log("Opa, ativação realizada!")
+    }
 
     return(
         <div>
             <p>Clique para disparar um evento:</p>
             <Button event={meuEvento}text="Primeiro Evento"/>
+            <Button event={segundoEvento}text="Segundo Evento"/>
             <button onClick={meuEvento}>Ativar!</button>
         </div>
     )
