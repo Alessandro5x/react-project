447b77cf89077a9b8f2420f6a10aec648f96ad3c
Alessandro <alessandro5@live.com>
2024-04-22 21:57:54 -0300

    feat: Iniciando a renderização de listas

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import Condicional from './components/Condicional';
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Renderização Condicional</h1>
+      <h1>Renderização de Listas</h1>
       <Condicional/>
     </div>
   );
